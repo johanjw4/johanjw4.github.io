3971e27941ed051009a6cb050fbfe46542c8f3fd
johanjw4 <70798540+johanjw4@users.noreply.github.com>
2025-04-09 15:40:51 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB5ae1DTNSbY2xD81O_o8T3RR2HD8HdBA4",
   authDomain: "johanjw4-github.firebaseapp.com",
@@ -7,4 +9,5 @@ const firebaseConfig = {
   appId: "1:715286980100:web:3e279004a9552f0362f751",
   measurementId: "G-G6SEDX6Q3V"
 };
+
 firebase.initializeApp(firebaseConfig);
