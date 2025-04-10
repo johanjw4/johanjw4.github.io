5548607b4663386ec020029cdc8533227371d9a6
johanjw4 <70798540+johanjw4@users.noreply.github.com>
2025-04-10 15:47:36 +0200

    Update and rename firebase-config.js to supabase-config.js
    the switch from firebase to supabase

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// supabase-config.js
+
+const supabaseUrl = 'https://hffehmdpqihsjkymfzyu.supabase.co'; // Your Supabase project URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmZmVobWRwcWloc2preW1menl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyOTIwNTEsImV4cCI6MjA1OTg2ODA1MX0.322my-svZkks8QV5k9lMm5C4WZzT9v_MqrGvhAk2Dt4';             // Your Supabase anon public API key
+
+// Export the configuration to be used in app.js
+export { supabaseUrl, supabaseKey };
