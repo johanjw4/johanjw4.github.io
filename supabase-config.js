5edef7d0b06b1278daf356beee9bc3f6ee942c95
johanjw4 <70798540+johanjw4@users.noreply.github.com>
2025-04-10 15:59:56 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,5 +3,8 @@
 const supabaseUrl = 'https://hffehmdpqihsjkymfzyu.supabase.co'; // Your Supabase project URL
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmZmVobWRwcWloc2preW1menl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyOTIwNTEsImV4cCI6MjA1OTg2ODA1MX0.322my-svZkks8QV5k9lMm5C4WZzT9v_MqrGvhAk2Dt4';             // Your Supabase anon public API key
 
-// Export the configuration to be used in app.js
-export { supabaseUrl, supabaseKey };
+// Initialize the Supabase client
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
+// Export the supabase instance
+export { supabase };
